955916a8681f0e891cbe2759968ba306c211db62
unknown <lennykmutua@gmail.com>
2019-11-09 08:38:31 +0300

    Add comments  on individual list items

--- imara/excel.py
+++ imara/excel.py
@@ -8,28 +8,28 @@ farmerData = []
 
 for row in range(3, 965):  # upto number of farmers
     # Each row in the spreadsheet has data for one member.
-    number = sheet['A' + str(row)].value
-    name = sheet['B' + str(row)].value
-    station = sheet['C' + str(row)].value
-    farmerData.append(sheet['AI' + str(row)].value)
-    rate = sheet['AJ' + str(row)].value
-    farmerData.append(sheet['AK' + str(row)].value)
-    farmerData.append(sheet['AL' + str(row)].value)
-    farmerData.append(sheet['AO' + str(row)].value)
-    farmerData.append(sheet['AP' + str(row)].value)
-    farmerData.append(sheet['AQ' + str(row)].value)
-    farmerData.append(sheet['AT' + str(row)].value)
-    farmerData.append(sheet['AU' + str(row)].value)
-    farmerData.append(sheet['AV' + str(row)].value)
-    farmerData.append(sheet['AX' + str(row)].value)
-    farmerData.append(sheet['AY' + str(row)].value)
-    farmerData.append(sheet['AZ' + str(row)].value)
-    farmerData.append(sheet['BA' + str(row)].value)
-    farmerData.append(sheet['BB' + str(row)].value)
-    farmerData.append(sheet['BC' + str(row)].value)
-    farmerData.append(sheet['BD' + str(row)].value)
-    farmerData.append(sheet['AW' + str(row)].value)
-    farmerData.append(sheet['BE' + str(row)].value)
+    number = sheet['A' + str(row)].value  # number
+    name = sheet['B' + str(row)].value  # name
+    station = sheet['C' + str(row)].value  # station
+    farmerData.append(sheet['AI' + str(row)].value)  # 0 totalKgs
+    rate = sheet['AJ' + str(row)].value  # rate
+    farmerData.append(sheet['AK' + str(row)].value)  # 1 grossAmount
+    farmerData.append(sheet['AL' + str(row)].value)  # 2 previousLoanBalance
+    farmerData.append(sheet['AO' + str(row)].value)  # 3 monthlyInstalment
+    farmerData.append(sheet['AP' + str(row)].value)  # 4 actualPayment
+    farmerData.append(sheet['AQ' + str(row)].value)  # 5 loanBalance
+    farmerData.append(sheet['AT' + str(row)].value)  # 6 storeLoan
+    farmerData.append(sheet['AU' + str(row)].value)  # 7 actualStoreRepayment
+    farmerData.append(sheet['AV' + str(row)].value)  # 8 storeLoanBalance
+    farmerData.append(sheet['AX' + str(row)].value)  # 9 previousSavings
+    farmerData.append(sheet['AY' + str(row)].value)  # 10 memberSavings
+    farmerData.append(sheet['AZ' + str(row)].value)  # 11 TotalMemberSavings
+    farmerData.append(sheet['BA' + str(row)].value)  # 12 previouRegistration
+    farmerData.append(sheet['BB' + str(row)].value)  # 13 totalRegistration
+    farmerData.append(sheet['BC' + str(row)].value)  # 14 previousShares
+    farmerData.append(sheet['BD' + str(row)].value)  # 15 totalShares
+    farmerData.append(sheet['AW' + str(row)].value)  # 16 totalDeductions
+    farmerData.append(sheet['BE' + str(row)].value)  # 17 netPay
     bank = sheet['BF' + str(row)].value
     accountNumber = sheet['BG' + str(row)].value
 
@@ -40,7 +40,7 @@ for row in range(3, 965):  # upto number of farmers
         accountNumber = 'N/A'
 
     data = f"""                IMARA DAIRY FARMERS CO-OPERATIVE SOCIETY LTD\n
-                TEL 0722 823 667/ 0723 688 721 / 0725 168 913\n
+                TEL 0722 823 667 / 0723 688 721 / 0725 168 913\n
 
                 MEMBER NUMBER: {number}\n
                 NAME: {name}\n
