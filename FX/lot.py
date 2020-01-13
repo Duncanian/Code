94664d072268f49e4885d43afa0a72afcb17cc4f
lenny kioko <lennykmutua@gmail.com>
2020-01-13 15:01:34 +0300

    Add final lot.py

--- FX/lot.py
+++ FX/lot.py
@@ -1,13 +1,17 @@
 #! python3
 """Calculates position size and risk reward ratio"""
 import sys
+import locale
+
+locale.setlocale(locale.LC_ALL, '')
 
 
 def calculate(loss, profit):
     risk_reward = round(profit / loss, 2)
-    max_risk = 95  # Ksh 9,500
-    volume = round(max_risk / (loss * 10), 2)
-    print(f"---- \nR/R ratio: {risk_reward} | Max win: {max_risk * risk_reward} | Volume: {volume} \n----")  # noqa E501
+    # max_risk = 500  # Ksh 5,000
+    max_risk = 150  # Ksh 15,000
+    volume = round(max_risk / (loss * 10.5), 2)
+    print(f"---- \nR/R ratio: {risk_reward} | Max win: {(max_risk * risk_reward) * 100 :n} | Volume: {volume} \n----")  # noqa E501
 
 
 if __name__ == "__main__":
